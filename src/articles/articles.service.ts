86ed4ee2b3db101ff93df94b27d653a109ea23b4
LedouxK <ledoux.kevin@yahoo.com>
2023-06-16 10:44:43 +0200

    Premier commit

---
+++ src/articles/articles.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { Article } from './interfaces/article.interface';
+
+@Injectable()
+export class ArticlesService {
+  constructor(
+    @InjectModel('Article') private readonly articleModel: Model<Article>,
+  ) {}
+
+  async create(createArticleDto: CreateArticleDto): Promise<Article> {
+    const createdArticle = new this.articleModel(createArticleDto);
+    return await createdArticle.save();
+  }
+
+  async findAll(): Promise<Article[]> {
+    return await this.articleModel.find().exec();
+  }
+}
